3eb6293a5801773be6dd070ac6a8c15670383308
nicobtech <nicolasbtech@gmail.com>
2021-11-09 22:28:19 -0300

    username resuelto

--- src/components/ReviewForm.vue
+++ src/components/ReviewForm.vue
@@ -54,7 +54,7 @@ export default {
     review() {
       const active_user = JSON.parse(sessionStorage.getItem('active_user'))
       axios.post( "http://localhost:5000/api/v1/reviews", {
-        user_name: active_user,
+        user_name: active_user.username,
         stars: this.picked,
         comment: this.comment,
         date: this.date,
